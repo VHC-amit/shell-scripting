c65d3ccffd7f6e1867c6534f51eb860952fb6ad8
VH-amit <amitt.gla@gmail.com>
2021-12-13 15:33:08 +0530

    new code added for variables print

--- 01-simple.sh
+++ 01-simple.sh
@@ -1,26 +1,8 @@
-ls -l
-
-a=5
-b=abc
-c=true
-
-function foo() {
-    echo first fucntion
-}
-
-#foo
 
 
-read -p 'Enter your age: ' age
 
-if [ -z ${age} ]; then
-    echo Please enter valid age
-  exit
-fi
 
-if [ ${age} -ge 18 ]; then
-    echo You are adult
-elif [ ${age} -le 18 ]; then
-    echo You are minor
-
+USER_UID=$(id -u)
+if [USER_UID != 0]; then
+  echo please login with the root user
 fi
